dfcaea420ac71100c09b55d7d07a5319285756e2
gyowoo113 <yereube@naver.com>
2023-07-31 15:37:52 +0900

    Feat : 상품/입고/출고 기능종류 설명(가이드) 출력위한 Title method 추가

--- productprj/src/main/java/co/micol/productprj/menu/MainMenu.java
+++ productprj/src/main/java/co/micol/productprj/menu/MainMenu.java
@@ -15,6 +15,38 @@ public class MainMenu {
 	private InboundService inboundService = new InboundServiceImpl();
 	private OutboundService outboundService = new OutboundServiceImpl();
 	
+	// title 
+	private void productTitle() {
+		System.out.println("== 상품 관리 ==");
+		System.out.println("= 1.상품 조회 =");
+		System.out.println("= 2.상품 등록 =");
+		System.out.println("= 3.상품 수정 =");
+		System.out.println("= 4.상품 삭제 =");
+		System.out.println("= 5.입고 관리 =");
+		System.out.println("= 6.출고 관리 =");
+		System.out.println("= 7.    종료 =");
+		System.out.println("=============");
+		System.out.println("원하는 작업번호를 입력하세요 >>");
+	}
+	
+	private void inboundTitle() {
+		System.out.println("===  입고 관리 ===");
+		System.out.println("= 1.전체내역 조회 =");
+		System.out.println("= 2.특정재고 조회 =");
+		System.out.println("= 3.   상품 입고 =");
+		System.out.println("================");
+		System.out.println("원하는 작업번호를 입력하세요?");
+	}
+	
+	private void outboundTitle() {
+		System.out.println("=== 출고 관리  ===");
+		System.out.println("= 1.전체내역 조회 =");
+		System.out.println("= 2.특정재고 조회 =");
+		System.out.println("= 3.   상품 출고 =");
+		System.out.println("================");
+		System.out.println("원하는 작업번호를 입력하세요?");
+	}
+	
 	public void run() {
 		
 	}
