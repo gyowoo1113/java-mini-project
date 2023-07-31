91846024536178ae04f53320968e2672e7793484
gyowoo113 <yereube@naver.com>
2023-07-31 11:46:05 +0900

    Add, Feat : properties 활용, driver 확인 및 getConnection

--- productprj/src/main/java/co/micol/productprj/common/DataSource.java
+++ productprj/src/main/java/co/micol/productprj/common/DataSource.java
@@ -10,12 +10,48 @@ import java.util.Properties;
 public class DataSource {
 	private static DataSource instance = new DataSource();
 	private Connection connection = null;
+	private String driver;
+	private String url;
+	private String user;
+	private String password;
 	
 	private DataSource() {
-
+		getProperties();
+	
+		try {
+			Class.forName(driver);
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	private void getProperties() {
+		String resource = "/database.properties";
+		Properties properties = new Properties();
+		
+		try {
+			InputStream reader = getClass().getResourceAsStream(resource);
+			properties.load(reader);
+			
+			driver = properties.getProperty("driver");
+			url = properties.getProperty("url");
+			user = properties.getProperty("user");
+			password = properties.getProperty("password");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 	
 	public static DataSource getInstance() {
 		return instance;
+	}
+	
+	public Connection getConnection() {
+		try {
+			connection = DriverManager.getConnection(url, user, password);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return connection;
 	}
 }
